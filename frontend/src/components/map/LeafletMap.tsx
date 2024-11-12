df8b9acbbd9935fba9fa481e4cf53247fd0bdd9c
Belinda Marion Kobusingye <46527380+Codebmk@users.noreply.github.com>
2024-11-12 05:48:58 +0300

    updated map language and leaflet version

--- frontend/src/components/map/LeafletMap.tsx
+++ frontend/src/components/map/LeafletMap.tsx
@@ -636,8 +636,8 @@ const LeafletMap: React.FC = () => {
         style={{ height: "100vh", width: "100%" }}
       >
         <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}"
+          attribution='Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
         />
         <SearchControl defaultCenter={defaultCenter} defaultZoom={defaultZoom} />
         <MapNodes onLoadingChange={setLoadingState} />
