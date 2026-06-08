5dac4dce1fbee1893c7de94918410b3454a1e022
wabinyai <rajacastro@gmail.com>
2026-06-08 13:25:52 +0300

    sw

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -4,5 +4,11 @@ self.addEventListener("install", () => {
 })
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(self.registration.unregister())
+  event.waitUntil(
+    (async () => {
+      const keys = await caches.keys()
+      await Promise.all(keys.map((key) => caches.delete(key)))
+      await self.registration.unregister()
+    })()  
+  )
 })
